3942034cff7ed73bae95dd40168448b29118cb74
Rodionov Aleksandr <allrod@mail.ru>
2018-06-07 16:57:52 +0300

    update html

--- shoppinglist-frontend/src/App.js
+++ shoppinglist-frontend/src/App.js
@@ -3,34 +3,32 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="col-md-6 offset-md-3">
-          <h1>Shopping lists</h1>
-          <ul className="list-unstyled">
-            <li>
-              <p>
-                <a href="/itemsList/1" className="btn btn-outline-secondary">Shopping list 1</a>
-                <a href="/editItemsList/1" className="btn btn-warning btn-sm">Edit</a>
-                <a href="" className="btn btn-danger btn-sm">Delete</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                <a href="/itemsList/2" className="btn btn-outline-secondary">Shopping list 2</a>
-                <a href="/editItemsList/2" className="btn btn-warning btn-sm">Edit</a>
-                <a href="" className="btn btn-danger btn-sm">Delete</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                <a href="/itemsList/3" className="btn btn-outline-secondary">Shopping list 3</a>
-                <a href="/editItemsList/3" className="btn btn-warning btn-sm">Edit</a>
-                <a href="" className="btn btn-danger btn-sm">Delete</a>
-              </p>
-            </li>
-          </ul>
-          <a href="/addItemsList" className="btn btn-success btn-sm">Add</a></div>
-      </div>
+      <div className="col-md-6 offset-md-3">
+        <h1>Shopping lists</h1>
+        <ul className="list-unstyled">
+          <li>
+            <p>
+              <a href="/itemsList/1" className="btn btn-outline-secondary">Shopping list 1</a>
+              <a href="/editItemsList/1" className="btn btn-warning btn-sm">Edit</a>
+              <a href="" className="btn btn-danger btn-sm">Delete</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/itemsList/2" className="btn btn-outline-secondary">Shopping list 2</a>
+              <a href="/editItemsList/2" className="btn btn-warning btn-sm">Edit</a>
+              <a href="" className="btn btn-danger btn-sm">Delete</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/itemsList/3" className="btn btn-outline-secondary">Shopping list 3</a>
+              <a href="/editItemsList/3" className="btn btn-warning btn-sm">Edit</a>
+              <a href="" className="btn btn-danger btn-sm">Delete</a>
+            </p>
+          </li>
+        </ul>
+        <a href="/addItemsList" className="btn btn-success btn-sm">Add</a></div>
     );
   }
 }

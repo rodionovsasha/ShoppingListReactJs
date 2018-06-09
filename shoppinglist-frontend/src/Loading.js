e98b0d051868fb9a6ecae786e0c171522e2f44ec
Rodionov Aleksandr <allrod@mail.ru>
2018-06-09 14:31:26 +0300

    Add ErrorAlert and Loading components

--- shoppinglist-frontend/src/Loading.js
+++ shoppinglist-frontend/src/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Loading extends React.Component {
   render() {
-    return <div className="col-md-6 offset-md-3">Loading...</div>
+    return <div className="col-md-6 offset-md-3">Loading...</div>;
   }
 }
 
